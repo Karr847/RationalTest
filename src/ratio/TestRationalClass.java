811bfedf2324cb72dbe9755e8bdf3ee999fbd549
Axmedarr <Axmedarr>
2021-09-29 14:40:10 +0100

    Initial commit

---
+++ src/ratio/TestRationalClass.java
@@ -0,0 +1,13 @@
+package ratio;
+
+public class TestRationalClass {
+
+
+    public static void main(String[] args) {
+        int i=190;
+
+
+        System.out.println(Integer.toString(i,2));
+    }
+
+}
